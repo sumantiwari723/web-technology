fdaf0f37d16330850cc05761c001093b1e28d2b6
sumantiwari723 <81008569+sumantiwari723@users.noreply.github.com>
2021-07-27 18:15:38 +0545

    Create dashboard.php

---
+++ lab9/dashboard.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome</title>
+</head>
+<body>
+<h1>You are logged In </h1>
+<form action="login.php" method="post">
+	<input type="hidden" name="reset">
+	<input type="submit" value="Log Out"/>
+</form>
+</body>
+</html>
