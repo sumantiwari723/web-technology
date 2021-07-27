c9c506496a5e7c0c5d7c0c9703986c72d9c5bfde
sumantiwari723 <81008569+sumantiwari723@users.noreply.github.com>
2021-07-27 18:16:20 +0545

    Create home.php

---
+++ lab9/home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Welcome</title>
+</head>
+<body>
+<ul>
+  <li>
+    <a href="login.php">Login</a><br>
+  </li>
+  <li>
+    <a href="register.php">Register</a>
+  </li>
+</ul>
+</body>
+</html>
